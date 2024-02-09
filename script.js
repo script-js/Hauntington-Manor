9c987a9573a2d7d3963fdfefa20672d7fd7670f9
script-js <134544514+script-js@users.noreply.github.com>
2024-02-09 14:32:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// Assignments
 
+var roomPicker = ["bed","living","bed2","lounge","baseM"]
+var charPicker = ["Betty","Anita","Nicholas","Theodore","Pamala"]
+
+// Rooms
+var rooms = {
+  "bed": false,
+  "living": false,
+  "bed2": false,
+  "lounge": false,
+  "baseM": false,
+  "attic": false,
+  "kitchen": false
+}
+
+// Suspects
+var suspects = {
+  "Betty": false,
+  "Anita": false,
+  "Nicholas": false,
+  "Theodore": false,
+  "Pamala": false
+}
+
+function initalAssignment() {
+  var murderer = charPicker[(Math.floor(Math.random() * charPicker.length))];
+  var mroom = roomPicker[(Math.floor(Math.random() * roomPicker.length))];
+  suspects[murderer] = true;
+  rooms[mroom] = true;
+}
