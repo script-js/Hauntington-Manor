1c538e6981c9996745cb353b5aa81ffe583a324a
script-js <134544514+script-js@users.noreply.github.com>
2024-02-11 17:35:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ function spawnClue(roomid) {
   newclue.classList = "invisiclue";
   newclue.style = details;
   newclue.id = "clue" +  Math.ceil(Math.random());
-  intJSON[newclue.id] = setInterval("if (touches(" + newclue.id + ")) {clearInterval(intJSON." + newclue.id + ");alert()}",1)
+  intJSON[newclue.id] = setInterval("if (touches(" + newclue.id + ")) {clearInterval(intJSON." + newclue.id + ");" + newclue.id + ".remove()}",1)
   clueCont.appendChild(newclue)
 }
 
