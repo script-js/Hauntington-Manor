efca56f0080c82f474abaa67aec9d87591c20044
script-js <134544514+script-js@users.noreply.github.com>
2024-02-18 14:15:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ function spawnClue(roomid) {
   newclue.style = details;
   newclue.id = "clue" +  Math.ceil(Math.random() * 1000);
   intJSON[newclue.id] = setInterval("if (touches(" + newclue.id + ")) {" + newclue.id + ".remove();clueAct('" + roomid + "'," + mcdata + ")};",1)
-  clueCont.appendChild(newclue)
+  imgFCont.appendChild(newclue)
 }
 
 function clueAct(roomid) {
